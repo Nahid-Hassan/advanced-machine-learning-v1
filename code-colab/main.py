76c6e2cde2e20ffefffd6bca8cf66e7afd9cf7a0
Nahid-Hassan <nahid.cseru@gmail.com>
2021-04-05 21:53:45 +0600

    interactive matplotlib - animation

--- code-colab/main.py
+++ code-colab/main.py
@@ -1,39 +1,31 @@
+import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
-from itertools import count
 
-actual = 6
+def plot_time_series(time, values, label):
+    plt.figure(figsize=(10,6))
+    plt.plot(time, values)
+    plt.xlabel("Time", fontsize=20)
+    plt.ylabel("Value", fontsize=20)
+    plt.title(label, fontsize=20)
+    plt.grid(True)
+    plt.show()
 
-posible_pred = list(range(1, 12, 1))
+# time = np.arange(100)
+# values = time*0.4
+# plot_time_series(time, values, "Upward Trend")
 
-b = list(range(-12, 12, 1)) * 10
+# Just a random pattern
+time = np.arange(50)
+values = np.where(time < 10, time**3, (time-9)**2)
+# Repeat the pattern 5 times
+seasonal = []
+for i in range(5):
+    for j in range(50):
+        seasonal.append(values[j])
+# Plot
+time_seasonal = np.arange(250)
 
-index = iter(b)
-
-cnt = 0
-
-def animate(i):
-    x = [(actual - posible) * -1 for posible in posible_pred]
-    y = [((actual - posible) * -1) ** 2 for posible in posible_pred]
-
-    global cnt
-    cnt = cnt + 1
-
-    c = 0
-    if cnt >= 13:
-        c = next(index)
-        c = c * -1
-    else:
-        c = next(index)
-    a = []
-    a.append(c)
-    print(a)
-
-    plt.cla()
-    plt.plot(x, y, 'go', markevery=a)
-    plt.plot(x, y)
-
-
-ani = FuncAnimation(plt.gcf(), animate, interval=1000)
-plt.tight_layout()
-plt.show()
+noise = np.random.randn(250)*100
+seasonal += noise
+time_seasonal = np.arange(250)
+plot_time_series(time_seasonal, seasonal, label="Seasonality with Noise")
