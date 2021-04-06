b21fd021a0f09b9d35fdab8ab959422e5b38b127
Nahid-Hassan <nahid.cseru@gmail.com>
2021-04-06 20:07:46 +0600

    neural network gradient descent

--- code-colab/main.py
+++ code-colab/main.py
@@ -1,31 +1,50 @@
-import numpy as np
+# https://youtu.be/sDv4f4s2SB8
+
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import math
+import numpy as np
+from itertools import count
 
-def plot_time_series(time, values, label):
-    plt.figure(figsize=(10,6))
-    plt.plot(time, values)
-    plt.xlabel("Time", fontsize=20)
-    plt.ylabel("Value", fontsize=20)
-    plt.title(label, fontsize=20)
-    plt.grid(True)
-    plt.show()
+x = [.5, 2.3, 2.9]
+y = [1.4, 1.9, 3.2]
 
-# time = np.arange(100)
-# values = time*0.4
-# plot_time_series(time, values, "Upward Trend")
+plt.scatter(x, y, marker='o')
 
-# Just a random pattern
-time = np.arange(50)
-values = np.where(time < 10, time**3, (time-9)**2)
-# Repeat the pattern 5 times
-seasonal = []
-for i in range(5):
-    for j in range(50):
-        seasonal.append(values[j])
-# Plot
-time_seasonal = np.arange(250)
+def sum_of_squared_residuals(x_data, y_data, intercept, slope):
+    residuals = 0
+    predicted = []
 
-noise = np.random.randn(250)*100
-seasonal += noise
-time_seasonal = np.arange(250)
-plot_time_series(time_seasonal, seasonal, label="Seasonality with Noise")
+    # in_ =list(intercept())
+    # print(in_)
+
+    for x, y in zip(x_data, y_data):
+        pred = (intercept + (slope * x))
+        predicted.append(pred)
+        residuals += (y - pred)
+
+    return (residuals, predicted)
+
+
+# sum_of_squared_residuals(x, y, intercept=0, slope=.64)
+a = np.arange(-5, 5, .5)
+index = iter(a)
+
+x_val = np.arange(-1, 3, .1)
+y_val = [sum_of_squared_residuals(x, y, intercept=c, slope=.64)[0] ** 2 for c in x_val]
+
+def animate(i):
+    plt.cla()
+
+    plt.scatter(x, y)
+    intercept = 0
+    r, y_hat = sum_of_squared_residuals(x, y, intercept=intercept, slope=.64)
+    print(abs(r), y_hat)
+    l = 'r: ' + str(round(abs(r), 2)) + ', intercept: ' + str(abs(intercept))
+    plt.plot(x, y_hat, label=l)
+    plt.plot(x_val, y_val)
+    plt.legend()
+
+
+ani = FuncAnimation(plt.gcf(), animate, interval=500)
+plt.show()
